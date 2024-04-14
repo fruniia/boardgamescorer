6d1e3778904cf489df11cbc31917f357ef682826
Anna Nilsson-Niia <annanilssonniia@gmail.com>
2024-04-14 16:03:35 +0200

    Add DBContext

---
+++ BoardGameScoring.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using BoardGameScoring.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoardGameScoring.Infrastructure.Data;
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+	public DbSet<Game> Games { get; set; }
+	public DbSet<Player> Players { get; set; }
+	public DbSet<Score> Scores { get; set; }
+	public DbSet<HighScore> HighScores { get; set; }
+}
