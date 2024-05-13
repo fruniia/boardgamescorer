713f8684821781028986bbe602d913278d35eb52
Anna Nilsson-Niia <annanilssonniia@gmail.com>
2024-05-13 15:36:15 +0200

    Add GlobalUsing

--- BoardGameScorer.WebUI.Server/Components/Pages/GameList.razor.cs
+++ BoardGameScorer.WebUI.Server/Components/Pages/GameList.razor.cs
@@ -1,6 +1,3 @@
-using BoardGameScorer.Domain.Entities;
-using Microsoft.AspNetCore.Components;
-
 namespace BoardGameScorer.WebUI.Server.Components.Pages;
 public partial class GameListBase : ComponentBase
 {
@@ -32,6 +29,6 @@ public partial class GameListBase : ComponentBase
 			Description = "With elegantly simple gameplay, Ticket to Ride can be learned in under 15 minutes. Players collect cards of various types of train cars they then use to claim railway routes in North America. The longer the routes, the more points they earn. Additional points come to those who fulfill Destination Tickets – goal cards that connect distant cities; and to the player who builds the longest continuous route.\r\n\r\n\"The rules are simple enough to write on a train ticket – each turn you either draw more cards, claim a route, or get additional Destination Tickets,\" says Ticket to Ride author, Alan R. Moon. \"The tension comes from being forced to balance greed – adding more cards to your hand, and fear – losing a critical route to a competitor.\"\r\n\r\nTicket to Ride continues in the tradition of Days of Wonder's big format board games featuring high-quality illustrations and components including: an oversize board map of North America, 225 custom-molded train cars, 144 illustrated cards, and wooden scoring markers."
 		};
 
-		Games = new List<Game> { game1, game2, game3 };
+		Games = [game1, game2, game3];
 	}
 }
---
+++ BoardGameScorer.WebUI.Server/GlobalUsing.cs
@@ -0,0 +1,4 @@
+﻿global using BoardGameScorer.Domain.Entities;
+global using BoardGameScorer.Domain.Enums;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Forms;
