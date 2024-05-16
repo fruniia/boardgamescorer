26ef46192d987daa467d1c922c5f111d1491dfd3
Anna Nilsson-Niia <annanilssonniia@gmail.com>
2024-05-16 12:52:02 +0200

    Change name on Registration to KingDominoGame and move it to its own class
    Add model to KingDominoScore

---
+++ BoardGameScorer.Domain/Models/KingDominoGame.cs
@@ -0,0 +1,13 @@
+﻿namespace BoardGameScorer.Domain.Models;
+
+public class KingDominoGame
+{
+	[Required]
+	public string? Terrain { get; set; }
+	[Required]
+	[Range(0, 6, ErrorMessage = $"{nameof(Crown)} must be between 0 and 6")]
+	public int Crowns { get; set; }
+	[Required]
+	[Range(1, 24, ErrorMessage = $"{nameof(Tiles)} must be between 1 and 24")]
+	public int Tiles { get; set; }
+}
--- BoardGameScorer.WebUI.Server/Components/Pages/KingDomino.razor.cs
+++ BoardGameScorer.WebUI.Server/Components/Pages/KingDomino.razor.cs
@@ -2,20 +2,27 @@ namespace BoardGameScorer.WebUI.Server.Components.Pages;
 
 public partial class KingDomino
 {
-	private EditContext? EditContext;
-	private GameScore gameScore = new GameScore();
+	private EditContext? _editContext;
+
+	[SupplyParameterFromForm]
+	public KingDominoGame? Model { get; set; }
+	private List<KingDominoGame> registration = new();
+	string[] Terrains = Terrain.GetNames(typeof(Terrain));
+	int[] Crowns = (int[])TileWithCrown.GetValues(typeof(TileWithCrown));
 
 	protected override void OnInitialized()
 	{
-		EditContext = new EditContext(gameScore);
+		Model ??= new();
+		_editContext = new(Model);
 		base.OnInitialized();
 	}
 
-	public class GameScore
+	private void HandleSubmit()
 	{
-		public Terrain Terrain { get; set; }
-		public Crown Crown { get; set; }
-		public TileWithCrown TileWithCrown { get; set; }
-		public int Tiles { get; set; }
+		if (Model!.Tiles > 0)
+		{
+			registration.Add(Model);
+			Model = new();
+		}
 	}
 }
