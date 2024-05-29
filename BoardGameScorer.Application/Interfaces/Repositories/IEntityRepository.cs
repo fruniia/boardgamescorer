13041cfe7ea9244a75091812616614f724e98067
Anna Nilsson-Niia <annanilssonniia@gmail.com>
2024-05-29 18:46:42 +0200

    Update and move IRepositories

---
+++ BoardGameScorer.Application/Interfaces/Repositories/IEntityRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace BoardGameScorer.Application.Interfaces.Repositories;
+public interface IEntityRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+    Task<List<T>> GetAllAsync();
+    Task AddAsync(T value);
+    Task UpdateAsync(T value);
+    Task DeleteAsync(int id);
+}
---
+++ BoardGameScorer.Application/Interfaces/Repositories/IPlayerRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace BoardGameScorer.Application.Interfaces.Repositories;
+public interface IPlayerRepository
+{
+    List<Player> GetAllAsync(int playSessionId);
+    Task<Player> GetByIdAsync(int playerId);
+    Task UpdateAsync(Player player);
+}
